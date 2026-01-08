ee0a7c91276437f3ef1592402be6bedb71958793
Zack Genia-Hajithemistou <zackgenia-hajithemistou@mac.lan>
2026-01-08 01:47:55 +0000

    Export Live page

--- client/src/pages/index.ts
+++ client/src/pages/index.ts
@@ -1,3 +1,4 @@
 export { SquadBuilder } from './SquadBuilder';
 export { Recommendations } from './Recommendations';
 export { FixtureTracker } from './FixtureTracker';
+export { default as Live } from './Live';
